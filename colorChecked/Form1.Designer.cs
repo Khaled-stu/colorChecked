1f7df45cabc6b45b905858fb24240bc6201f2782
pc <pc@DESKTOP-DIEFINS>
2024-10-08 08:23:37 +0300

    Add project files.

---
+++ colorChecked/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace colorChecked
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.rdoYellow = new System.Windows.Forms.RadioButton();
+            this.rdoGreen = new System.Windows.Forms.RadioButton();
+            this.rdoBlue = new System.Windows.Forms.RadioButton();
+            this.rdoRed = new System.Windows.Forms.RadioButton();
+            this.chkGreen = new System.Windows.Forms.CheckBox();
+            this.chkBlue = new System.Windows.Forms.CheckBox();
+            this.chkRed = new System.Windows.Forms.CheckBox();
+            this.chkYellow = new System.Windows.Forms.CheckBox();
+            this.radioGreen = new System.Windows.Forms.RadioButton();
+            this.radioRed = new System.Windows.Forms.RadioButton();
+            this.radioYellow = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.radioYellow);
+            this.groupBox1.Controls.Add(this.radioRed);
+            this.groupBox1.Controls.Add(this.radioGreen);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.rdoYellow);
+            this.groupBox1.Controls.Add(this.rdoGreen);
+            this.groupBox1.Controls.Add(this.rdoBlue);
+            this.groupBox1.Controls.Add(this.rdoRed);
+            this.groupBox1.Controls.Add(this.chkGreen);
+            this.groupBox1.Controls.Add(this.chkBlue);
+            this.groupBox1.Controls.Add(this.chkRed);
+            this.groupBox1.Controls.Add(this.chkYellow);
+            this.groupBox1.Location = new System.Drawing.Point(57, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(662, 357);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "what is the color of the grass\r\n";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.SystemColors.Info;
+            this.btnReset.Location = new System.Drawing.Point(410, 26);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(174, 45);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // rdoYellow
+            // 
+            this.rdoYellow.AutoSize = true;
+            this.rdoYellow.Location = new System.Drawing.Point(423, 287);
+            this.rdoYellow.Name = "rdoYellow";
+            this.rdoYellow.Size = new System.Drawing.Size(54, 17);
+            this.rdoYellow.TabIndex = 7;
+            this.rdoYellow.TabStop = true;
+            this.rdoYellow.Text = "yellow";
+            this.rdoYellow.UseVisualStyleBackColor = true;
+            this.rdoYellow.CheckedChanged += new System.EventHandler(this.rdoYellow_CheckedChanged);
+            // 
+            // rdoGreen
+            // 
+            this.rdoGreen.AutoSize = true;
+            this.rdoGreen.Location = new System.Drawing.Point(423, 246);
+            this.rdoGreen.Name = "rdoGreen";
+            this.rdoGreen.Size = new System.Drawing.Size(52, 17);
+            this.rdoGreen.TabIndex = 6;
+            this.rdoGreen.TabStop = true;
+            this.rdoGreen.Text = "green";
+            this.rdoGreen.UseVisualStyleBackColor = true;
+            this.rdoGreen.CheckedChanged += new System.EventHandler(this.rdoGreen_CheckedChanged);
+            // 
+            // rdoBlue
+            // 
+            this.rdoBlue.AutoSize = true;
+            this.rdoBlue.Location = new System.Drawing.Point(423, 207);
+            this.rdoBlue.Name = "rdoBlue";
+            this.rdoBlue.Size = new System.Drawing.Size(45, 17);
+            this.rdoBlue.TabIndex = 5;
+            this.rdoBlue.TabStop = true;
+            this.rdoBlue.Text = "blue";
+            this.rdoBlue.UseVisualStyleBackColor = true;
+            this.rdoBlue.CheckedChanged += new System.EventHandler(this.rdoBlue_CheckedChanged);
+            // 
+            // rdoRed
+            // 
+            this.rdoRed.AutoSize = true;
+            this.rdoRed.Location = new System.Drawing.Point(423, 161);
+            this.rdoRed.Name = "rdoRed";
+            this.rdoRed.Size = new System.Drawing.Size(40, 17);
+            this.rdoRed.TabIndex = 4;
+            this.rdoRed.TabStop = true;
+            this.rdoRed.Text = "red";
+            this.rdoRed.UseVisualStyleBackColor = true;
+            this.rdoRed.CheckedChanged += new System.EventHandler(this.rdoRed_CheckedChanged);
+            // 
+            // chkGreen
+            // 
+            this.chkGreen.AutoSize = true;
+            this.chkGreen.Location = new System.Drawing.Point(514, 288);
+            this.chkGreen.Name = "chkGreen";
+            this.chkGreen.Size = new System.Drawing.Size(53, 17);
+            this.chkGreen.TabIndex = 3;
+            this.chkGreen.Text = "green";
+            this.chkGreen.UseVisualStyleBackColor = true;
+            this.chkGreen.CheckedChanged += new System.EventHandler(this.chkGreen_CheckedChanged);
+            // 
+            // chkBlue
+            // 
+            this.chkBlue.AutoSize = true;
+            this.chkBlue.Location = new System.Drawing.Point(521, 247);
+            this.chkBlue.Name = "chkBlue";
+            this.chkBlue.Size = new System.Drawing.Size(46, 17);
+            this.chkBlue.TabIndex = 2;
+            this.chkBlue.Text = "blue";
+            this.chkBlue.UseVisualStyleBackColor = true;
+            this.chkBlue.CheckedChanged += new System.EventHandler(this.chkBlue_CheckedChanged);
+            // 
+            // chkRed
+            // 
+            this.chkRed.AutoSize = true;
+            this.chkRed.Location = new System.Drawing.Point(526, 208);
+            this.chkRed.Name = "chkRed";
+            this.chkRed.Size = new System.Drawing.Size(41, 17);
+            this.chkRed.TabIndex = 1;
+            this.chkRed.Text = "red";
+            this.chkRed.UseVisualStyleBackColor = true;
+            this.chkRed.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // chkYellow
+            // 
+            this.chkYellow.AutoSize = true;
+            this.chkYellow.Location = new System.Drawing.Point(526, 162);
+            this.chkYellow.Name = "chkYellow";
+            this.chkYellow.Size = new System.Drawing.Size(71, 17);
+            this.chkYellow.TabIndex = 0;
+            this.chkYellow.Text = "YELLOW";
+            this.chkYellow.UseVisualStyleBackColor = true;
+            this.chkYellow.CheckedChanged += new System.EventHandler(this.chkBlack_CheckedChanged);
+            // 
+            // radioGreen
+            // 
+            this.radioGreen.AutoSize = true;
+            this.radioGreen.Location = new System.Drawing.Point(56, 208);
+            this.radioGreen.Name = "radioGreen";
+            this.radioGreen.Size = new System.Drawing.Size(52, 17);
+            this.radioGreen.TabIndex = 10;
+            this.radioGreen.TabStop = true;
+            this.radioGreen.Text = "green";
+            this.radioGreen.UseVisualStyleBackColor = true;
+            this.radioGreen.CheckedChanged += new System.EventHandler(this.radioGreen_CheckedChanged);
+            // 
+            // radioRed
+            // 
+            this.radioRed.AutoSize = true;
+            this.radioRed.Location = new System.Drawing.Point(56, 162);
+            this.radioRed.Name = "radioRed";
+            this.radioRed.Size = new System.Drawing.Size(40, 17);
+            this.radioRed.TabIndex = 11;
+            this.radioRed.TabStop = true;
+            this.radioRed.Text = "red";
+            this.radioRed.UseVisualStyleBackColor = true;
+            this.radioRed.CheckedChanged += new System.EventHandler(this.radioRed_CheckedChanged);
+            // 
+            // radioYellow
+            // 
+            this.radioYellow.AutoSize = true;
+            this.radioYellow.Location = new System.Drawing.Point(56, 259);
+            this.radioYellow.Name = "radioYellow";
+            this.radioYellow.Size = new System.Drawing.Size(54, 17);
+            this.radioYellow.TabIndex = 12;
+            this.radioYellow.TabStop = true;
+            this.radioYellow.Text = "yellow";
+            this.radioYellow.UseVisualStyleBackColor = true;
+            this.radioYellow.CheckedChanged += new System.EventHandler(this.radioYellow_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "without else statement";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(419, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "with else statement";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkGreen;
+        private System.Windows.Forms.CheckBox chkBlue;
+        private System.Windows.Forms.CheckBox chkRed;
+        private System.Windows.Forms.CheckBox chkYellow;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.RadioButton rdoYellow;
+        private System.Windows.Forms.RadioButton rdoGreen;
+        private System.Windows.Forms.RadioButton rdoBlue;
+        private System.Windows.Forms.RadioButton rdoRed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioYellow;
+        private System.Windows.Forms.RadioButton radioRed;
+        private System.Windows.Forms.RadioButton radioGreen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ colorChecked/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace colorChecked
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkRed.Checked == true)
+            {
+                chkRed.ForeColor = Color.FromName("red");
+            }
+            else
+            {
+                chkRed.ForeColor = Color.FromName("black");
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //with else statment reset color
+            chkYellow.ForeColor = Color.Black;
+            chkBlue.ForeColor = Color.Black;
+            chkGreen.ForeColor = Color.Black;
+            chkRed.ForeColor = Color.Black;
+            
+            rdoBlue.ForeColor = Color.Black;
+            rdoGreen.ForeColor = Color.Black;
+            rdoRed.ForeColor = Color.Black;
+            rdoYellow.ForeColor = Color.Black;
+
+            // with else statment reset answer
+            chkYellow.Checked = false;
+            chkBlue.Checked = false;
+            chkGreen.Checked = false;
+            chkRed.Checked = false;
+
+            rdoBlue.Checked = false;
+            rdoGreen.Checked = false;
+            rdoRed.Checked = false;
+            rdoYellow.Checked = false;
+
+            //without else statment reset color
+            radioGreen.ForeColor = Color.FromName("black");
+            radioRed.ForeColor = Color.FromArgb(0,0,0);
+            radioYellow.ForeColor = Color.Black;
+
+            // without else statment reset answer
+            radioGreen.Checked = false;
+            radioYellow.Checked = false;
+            radioRed.Checked = false;
+
+
+
+        }
+
+        private void rdoRed_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoRed.Checked == true)
+            {
+                rdoRed.ForeColor = Color.FromName("red");
+            }
+            else
+            {
+                rdoRed.ForeColor = Color.FromName("black");
+            }
+        }
+
+        private void rdoBlue_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoBlue.Checked == true)
+            {
+                rdoBlue.ForeColor = Color.FromName("blue");
+            }
+            else
+            {
+                rdoBlue.ForeColor = Color.FromName("black");
+            }
+        }
+
+        private void rdoGreen_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoGreen.Checked == true)
+            {
+                rdoGreen.ForeColor = Color.FromName("green");
+            }
+            else
+            {
+                rdoGreen.ForeColor = Color.FromName("black");
+            }
+        }
+
+        private void rdoYellow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoYellow.Checked == true)
+            {
+                rdoYellow.ForeColor = Color.FromName("yellow");
+            }
+            else
+            {
+                rdoYellow.ForeColor = Color.FromName("black");
+            }
+        }
+
+        private void chkBlack_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkYellow.Checked == true)
+            {
+                chkYellow.ForeColor = Color.FromName("yellow");
+            }
+            else 
+            {
+                chkYellow.ForeColor = Color.FromName("black");   
+            }
+        }
+
+        private void chkBlue_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkBlue.Checked == true)
+            {
+                chkBlue.ForeColor = Color.FromName("blue");
+            }
+            else
+            {
+                chkBlue.ForeColor = Color.FromName("black");
+            }
+        }
+
+        private void chkGreen_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkGreen.Checked == true)
+            {
+                chkGreen.ForeColor = Color.FromArgb(0, 255, 0);
+            }
+            else
+            {
+                chkGreen.ForeColor = Color.FromName("black");
+            }
+        }
+
+        private void radioRed_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioRed.Checked == true)
+            {
+                radioRed.ForeColor = Color.FromName("red");
+            }
+            
+        }
+
+        private void radioGreen_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioGreen.Checked == true)
+            {
+                radioGreen.ForeColor = Color.FromName("green");
+            }
+        }
+
+        private void radioYellow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioYellow.Checked == true)
+            {
+                radioYellow.ForeColor = Color.FromName("yellow");
+            }
+        }
+    }
+    }
+
